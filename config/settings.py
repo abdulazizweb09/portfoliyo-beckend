69ca0da8a76a78e453c8ca86b2f4940ff4509927
abdulazizweb09 <xakimovxadyatillo92@gmail.com>
2026-08-28 20:02:15 +0500

    fix: Comment out STATIC_ROOT setting for static file management

--- config/settings.py
+++ config/settings.py
@@ -169,6 +169,6 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 AUTH_USER_MODEL = "main.UserProfile"
