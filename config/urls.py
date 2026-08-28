ed91c10d4f13943e4ad256245b5a5aa19faf122e
abdulazizweb09 <xakimovxadyatillo92@gmail.com>
2026-08-28 19:53:09 +0500

    .

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
     re_path(
-        r"^dock/$",
+        r"^$",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
